e2b03e0e95cbd3a993715ad80553a6adb040399c
C0rruptHe1ix7682 <crruptheix5@gmail.com>
2025-01-08 09:23:45 -0500

    Added notes and fixed errors

--- BPA Python Challenges/Classes_and_Objects.py
+++ BPA Python Challenges/Classes_and_Objects.py
@@ -12,3 +12,5 @@ Student2 = Student("Jack", 11, 89)
 
 print(Student1.display_info())
 print(Student2.display_info())
+
+#I first created the student class and created a name object, grade object, and percentage object. I then made a function to display the info to the reader that would tell you their name, grade level, and their grade percentage currently. Then I made two objects from the class, both of the students! They contained their names, grade, and percentage. I then printed their info and that they passed.
